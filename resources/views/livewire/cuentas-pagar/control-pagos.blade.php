c2d40f37d4b8c4d5d9c09b98c9cce23b5865c32c
Carlos Ruiz <131738990+CarlosRuiz-1996@users.noreply.github.com>
2024-07-03 14:05:12 -0600

    control de pagos

--- resources/views/livewire/cuentas-pagar/control-pagos.blade.php
+++ resources/views/livewire/cuentas-pagar/control-pagos.blade.php
@@ -13,7 +13,7 @@
                margin-left: 1.25rem; 
                margin-right: 1.25rem; 
                backdrop-filter: blur(5px);">
-            <h2 class="text-2xl font-bold text-white mb-4">Resumen de Costo por Producto</h2>
+            <h2 class="text-2xl font-bold text-white mb-4">Control de pagos</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <!-- Aumenté el gap a 6 para mayor separación -->
                 <div class="flex flex-col space-y-4">
@@ -24,9 +24,9 @@
                             class="p-2 border border-gray-300 rounded w-full" placeholder="Selecciona una estación">
                             <option value="">Selecciona una estación...</option>
                             @foreach ($estaciones as $estacion)
-                                @if ($estacion->IdEstacion == 153)
-                                    <option value="{{ $estacion->IdEstacion }}">{{ $estacion->NombreEstacion }}</option>
-                                @endif
+                                {{-- @if ($estacion->IdEstacion == 153) --}}
+                                <option value="{{ $estacion->IdEstacion }}">{{ $estacion->NombreEstacion }}</option>
+                                {{-- @endif --}}
                             @endforeach
                         </select>
                     </div>
@@ -58,22 +58,25 @@
                         </div>
                         <!-- Botón de búsqueda -->
                         <div class="flex flex-col space-y-3 flex-grow pl-2">
-                            <button class="bg-blue-500 text-white py-2 px-2 mt-8 rounded hover:bg-blue-700 w-full"
-                                wire:click='buscar'>Buscar</button>
+                            <x-button class="mt-6" wire:click='buscar'>Buscar</x-button>
                         </div>
                     </div>
-                    @if ($estacion_detalle)
-                        <div class="flex space-x-1 items-center">
+                    @if ($estaciond)
+                        <div class="flex space-x-1 items-center mt-5">
                             <div
                                 class="flex-grow p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                 <div>
-                                    <h2><b>Nombre:{{$estacion_detalle->NombreEstacion}}</b></h2>
+                                    <h2><b>Nombre:{{ $estaciond->nombre_emisor }}</b></h2>
                                 </div>
                                 <div>
-                                    <h2><b>RFC: {{$estacion_detalle->RFC}}</b></h2>
+                                    <h2><b>RFC: {{ $estaciond->rfc_emisor }}</b></h2>
                                 </div>
                                 <div>
-                                    <h2><b>Domicilio: {{$estacion_detalle->DireccionFiscal}}</b></h2>
+                                    <h2><b>Domicilio: XXXXXXXXXXXXXXXXXX</b></h2>
+                                </div>
+
+                                <div>
+                                    <x-button wire:click='openModal'>Detalles</x-button>
                                 </div>
                             </div>
                         </div>
@@ -83,15 +86,13 @@
 
             </div>
 
-            <!-- Nueva sección: Inventario y Adquisición -->
-            <h1 class="text-white mt-8">Detalle</h1>
+            <div class="grid grid-cols-3 md:grid-cols-3 gap-6 mt-3 mb-3">
+                <h1 class="text-white text-2xl">Detalle</h1>
+                <h1 class="text-white text-2xl ">Monto pagado: $ {{ number_format($monto_pagado, 3) }}</h1>
+                <h1 class="text-white text-2xl text-end">Total de facturas: {{ $total_facturas }}</h1>
+            </div>
 
 
-            <h1 class="text-white mt-8">Monto pagado: {{$monto_pagado}}</h1>
-            {{-- <div class="flex flex-col space-y-3 flex-grow pl-2">
-                <button class="bg-blue-500 text-white py-2 mt-6 rounded hover:bg-blue-700 w-full"
-                    wire:click='exportarExcel'>Exportar Excel</button>
-            </div> --}}
             <div class="mt-2">
                 <div class="overflow-x-auto shadow-md rounded-lg" wire:init='loadEstaciones'>
 
@@ -149,31 +150,35 @@
                                             {{ $detalle->combustible }}
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{ number_format($detalle->litros, 6) }}
+                                            {{ number_format($detalle->litros, 2) }}
 
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{ number_format($detalle->SubTotal, 6) }}
+                                            {{ number_format($detalle->SubTotal, 2) }}
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{ number_format($detalle->Total, 6) }}
+                                            {{ number_format($detalle->Total, 2) }}
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-green-500">
+                                            <b>{{ $detalle->estatus ? 'PAGADA' : 'PENDIENTE' }}</b>
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{ $detalle->estatus ? 'PAGADA' : 'PENDIENTE' }}
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            @if($detalle->TipoDeComprobante !="P")
-                                            <button wire:click="mostrarPdf('{{ asset('storage/archivos_descomprimidos/' . $detalle->estatus . '@1000000000XX0.pdf') }}')" class="text-blue-500 hover:text-blue-700"><span class="mr-1">
-                                                <i class="fas fa-file-pdf text-red-500"></i> <!-- Icono de PDF -->
-                                            </span></button>                                 
+                                            @if ($detalle->TipoDeComprobante != 'P')
+                                                <button
+                                                    wire:click="mostrarPdf('{{ asset('storage/archivos_descomprimidos/' . $detalle->estatus . '@1000000000XX0.pdf') }}')"
+                                                    class="text-blue-500 hover:text-blue-700"><span class="mr-1">
+                                                        <i class="fas fa-file-pdf text-red-500"></i>
+                                                        <!-- Icono de PDF -->
+                                                    </span></button>
                                             @else
                                                 Sin Archivo PDF
                                             @endif
-                                            <button wire:click='descargarXML({{$detalle->id}})'>
+                                            <button wire:click='descargarXML({{ $detalle->id }})'>
                                                 <span class="mr-1">
-                                                    <i class="fas fa-file-code text-green-500"></i> <!-- Icono de XML -->
+                                                    <i class="fas fa-file-code text-green-500"></i>
+                                                    <!-- Icono de XML -->
                                                 </span>
                                             </button>
                                         </td>
@@ -195,7 +200,8 @@
                         @else
                             <!-- Muestra un spinner mientras los datos se cargan -->
                             <div class="flex justify-center items-center h-40">
-                                <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-yellow-500">
+                                <div
+                                    class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-yellow-500">
                                 </div>
                             </div>
                         @endif
@@ -207,37 +213,166 @@
     </div>
 
 
-      <!-- Modal para mostrar PDF -->
-      @if($isOpen)
-      <div class="fixed z-10 inset-0 overflow-y-auto">
-          <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-              <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-                  <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-              </div>
+    <!-- Modal para mostrar PDF -->
+    @if ($isOpen)
+        <div class="fixed z-10 inset-0 overflow-y-auto">
+            <div class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
 
-              <!-- Este elemento es necesario para centrar el contenido del modal -->
-              <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <!-- Este elemento es necesario para centrar el contenido del modal -->
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-              <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                  <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                      <div class="sm:flex sm:items-start">
-                          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
-                              <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                  PDF Detalle
-                              </h3>
-                              <div class="mt-2">
-                                  <embed src="{{ $pdfUrl }}" type="application/pdf" class="w-full" height="500px" />
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                      <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm" wire:click="closeModal">
-                          Cerrar
-                      </button>
-                  </div>
-              </div>
-          </div>
-      </div>
-  @endif
+                <div
+                    class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                    PDF Detalle
+                                </h3>
+                                <div class="mt-2">
+                                    <embed src="{{ $pdfUrl }}" type="application/pdf" class="w-full"
+                                        height="500px" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <button type="button"
+                            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                            wire:click="closeModal">
+                            Cerrar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    {{-- modal para apartar productos y surtir --}}
+    <x-dialog-modal wire:model.live="open">
+        @slot('title')
+            <div class="px-6 py-4 items-center  bg-gray-100 overflow-x-auto shadow-md sm:rounded-lg">
+                <h1>Detalles de estación 
+                </h1>
+            </div>
+        @endslot
+        @slot('content')
+
+        @if ($estacion_detalle)
+        <div class="grid grid-cols-2 gap-6">
+            <div class="col-span-1">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Estación:{{ $estacion_detalle->estacion }}</b></h2>
+                </div>
+            </div>
+            <div class="col-span-1">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Numero Destino:{{ $estacion_detalle->NumeroDestino }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>PermisoCRE:{{ $estacion_detalle->PermisoCRE }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>SIIC:{{ $estacion_detalle->SIIC }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Nombre Estacion:{{ $estacion_detalle->NombreEstacion }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>RFC:{{ $estacion_detalle->RFC }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Direccion Fiscal:{{ $estacion_detalle->DireccionFiscal }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Correo Analista JR:{{ $estacion_detalle->CorreoAnalistaJR }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Contador:{{ $estacion_detalle->Contador }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Correo Contador:{{ $estacion_detalle->CorreoContador }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Analista CtaX Pagar:{{ $estacion_detalle->AnalistaCtaXPagar }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Correo CXP:{{ $estacion_detalle->CorreoCXP }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Analista CtaX Cobrar:{{ $estacion_detalle->AnalistaCtaXCobrar }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Correo CXC:{{ $estacion_detalle->CorreoCXC }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Secretaria:{{ $estacion_detalle->Secretaria }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Correo SCA:{{ $estacion_detalle->CorreoSCA }}</b></h2>
+
+                </div>
+            </div>
+            <div class="col-span-2">
+                <div class="relative z-0 w-full group">
+                    <h2><b>Descarga Contra cargos:{{ $estacion_detalle->DescargaContracargos }}</b></h2>
+
+                </div>
+            </div>
+        </div>
+        @endif
+            
+
+        @endslot
+        @slot('footer')
+            <x-secondary-button wire:click="clean">Cerrar</x-secondary-button>
+               
+        @endslot
+    </x-dialog-modal>
+
 </div>
